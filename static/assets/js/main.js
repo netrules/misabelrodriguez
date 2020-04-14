d434070da70950652646b34e51d16f7eb4a7977d
Santiago González <santiago.publicmail@gmail.com>
2020-04-14 01:44:50 +0000

    Modal pure jquery (-bootstrap req)

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -170,10 +170,28 @@
 			}
 		});
 
-    $(".image").on("click", function() {
-        $('#imagepreview').attr('src', $('.image > img').attr('src')); // here asign the image to the modal when the user click the enlarge link
-        $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
-    });
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = $('.myImg');
+var modalImg = $("#img01");
+//var captionText = document.getElementById("caption");
+$('.imageBind').click(function(){
+    modal.style.display = "block";
+    var newSrc = this.src;
+    modalImg.attr('src', newSrc);
+//    captionText.innerHTML = this.alt;
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
 
 
 })(jQuery);
