366ba44a2e9bdbb9ffe9d3f75b57b712a2bbcbae
Santiago González <santiago.publicmail@gmail.com>
2020-04-14 02:01:40 +0000

    Modal-close

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -188,7 +188,7 @@ $('.imageBind').click(function(){
 });
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("modal-close")[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
